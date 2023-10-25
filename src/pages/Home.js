fd7105cb438b3e67fe4ef458d60e0e2b29d3264b
Tommy Yang <tommy_yang621@hotmail.com>
2023-10-24 22:15:43 -0400

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
+import supabase from "../config/SupabaseClient"
+import { useEffect, useState } from "react"
+//components
+import SmoothieCard from "../component/SmoothieCard.js"
 const Home = () => {
+  const [fetcherror, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies')
+        .select()
+      if (error) {
+        setFetchError('could not fetch smoothies')
+        setSmoothies(null)
+        console.log(error)
+      }
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+    fetchSmoothies()
+  }, [])
   return (
+
     <div className="page home">
-      <h2>Home</h2>
+      {fetcherror && (<p>{fetcherror}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order by button*/}
+          <div className='smoothie-grid'>        
+            {smoothies.map(smoothie => (
+            <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+          ))}</div>
+        </div>)}
     </div>
   )
 }
